bde0a161059a1134dbb3eaad44cd32b2e50db85b
bigdataBTC <604416495@qq.com>
2021-05-13 17:13:30 +0800

    1

--- app/src/main/java/com/xinmang/mvpdatabindingdemo/base/baseadapter/OnItemClickLisrener.java
+++
@@ -1,9 +0,0 @@
-package com.xinmang.mvpdatabindingdemo.base.baseadapter;
-
-/**
- * Created by lipei on 2018/1/29.
- */
-
-public interface OnItemClickLisrener<T> {
-    void onClick(T t,int postion);
-}
